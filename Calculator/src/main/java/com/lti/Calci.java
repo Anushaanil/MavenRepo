86b26055e3d6008cdb7fd8eb65a78b02ba9d1da7
Anusha Shet <anushaanil44@gmail.com>
2020-12-21 15:16:37 +0530

    modified prod to div method

--- Calculator/src/main/java/com/lti/Calci.java
+++ Calculator/src/main/java/com/lti/Calci.java
@@ -8,13 +8,13 @@ public class Calci {
 	public static int subs(int n1, int n2) {
 		return n1-n2;
 	}
-	public static int prod(int n1, int n2) {
-		return n1*n2;
+	public static int div(int n1, int n2) {
+		return n1/n2;
 	}
 	public static void main(String[] args) {
 		System.out.println("Sum= "+add(10,20));
 		System.out.println("Difference= "+subs(90,10));
-		System.out.println("Product= "+prod(1,10));
+		System.out.println("division= "+div(100,10));
 	}
 
 }
